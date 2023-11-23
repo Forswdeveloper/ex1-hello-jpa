d1c96d8b939ce569233114cc998c24f2373c4184
jdi0901 <jdi0901@naver.com>
2023-11-23 22:19:21 +0900

    commit

--- src/main/java/hellojpa/jpaMain.java
+++ src/main/java/hellojpa/jpaMain.java
@@ -35,16 +35,66 @@ public class jpaMain {
             //JPA에서 쿼리를 만들 때 테이블 대상으로 만들지 않는다. 멤버 객채를 대상으로 함.
             //select m -> 객체 대상
             //엄청난 장점이 존재. 테이블을 대상으로 쿼리를 작성하면 테이블에 종속적인 쿼리가 되어버리는데 객체를 중심으로 작성하면 객체 중심적 쿼리가 됨.
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//                    .setFirstResult(5)
+//                    .setMaxResults(8)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+            //비영속 상태
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+            //영속
+//            System.out.println("=== Before ===");
+//            em.persist(member);
+//            System.out.println("=== After ===");
+//            === Before ===
+//            === After ===
+//                                Hibernate:
+//                /* insert for
+//                    hellojpa.Member */insert
+//                                into
+//                        Member (name,Id)
+//                        values
+//                                (?,?)
+            //영속 상태가 된다고해서 바로 쿼리가 실행되지 않음
 
-            //jpa에서는 트랜잭션안에서 작업을 해야함.
+            //1차 캐쉬에서 조회
+//            Member findMember1 = em.find(Member.class, 101L);  //첫번째 조회시에는 쿼리가 실행
+//            Member findMember2 = em.find(Member.class, 101L);  //두번째 조회시에는 1차캐쉬에서 조회
+//
+//            System.out.println("findMember.id = "+findMember1.getId());
+//            System.out.println("findMember.name = "+findMember1.getName());
+//            //영속 엔티티의 동일성 보장
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+            //트랜잭션 커밋 시점에 영속성 컨텍스트에 있는것들이 쿼리가 실행됨.
+
+            // jpa에서는 트랜잭션안에서 작업을 해야함.
+            //엔티티 등록 : 트랜잭션을 지원하는 쓰기 지연
+//            Member member1 = new Member(150L,"A");
+//            Member member2 = new Member(160L,"B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("=================");
+
+            //엔티티 수정 (변경 감지) flush() -> 엔티티 스냅샷과 비교 -> update 생성 -> flush() -> commit
+            // flush() : 영속성 컨텍스트의 변경 내용을 데이터베이스에 반영
+//            Member member = em.find(Member.class,150L);
+//            member.setName("ZZZZZ");
+//            System.out.println("=================");
+            Member member = new Member(200L,"member200");
+            em.persist(member);
+            em.flush();
+
+            System.out.print("==================");
+
+            //쿼리 실행 시점
             tx.commit();
         }catch (Exception e) {
             tx.rollback();
